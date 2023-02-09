ebd466434c6e847fdf3a2cd39d2bb1408a6277a2
= <zm275@byu.edu>
2023-02-09 09:00:12 -0700

    Add project files.

---
+++ Mission6_zm275/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission6_zm275.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6_zm275.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+        public IActionResult Form()
+        {
+            FormModel model = new FormModel();
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission6_zm275/Models/Form.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6_zm275.Models
+{
+    public class FormModel
+    {
+        
+        public string LentTo { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public int Year { get; set; }
+        public string Notes { get; set;}
+        public bool Edited { get; set; }
+        [Required]
+        public string SelectedItem { get; set; }
+        public IEnumerable<SelectListItem> Items { get; set; }
+
+        public FormModel()
+        {
+            Items = new List<SelectListItem>
+        {
+            new SelectListItem {Value = "1", Text = "G"},
+            new SelectListItem {Value = "2", Text = "PG"},
+            new SelectListItem {Value = "3", Text = "PG-13"},
+            new SelectListItem {Value = "4", Text = "R"},
+        };
+        }
+    }
+}
